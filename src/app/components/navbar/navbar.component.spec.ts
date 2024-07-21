ace60e798cc9eaef51a4a1023c41b9fffbb08715
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-07-22 00:00:41 +0200

    added test for navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { ModeService } from '../../services/mode/mode.service';
+import { signal } from '@angular/core';
+
+class MockModeService{
+  darkMode = signal<string>('null');
+
+  handleMode(){
+    this.darkMode.update((value)=>(value === 'dark'? 'null':'dark'))
+  }
+}
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let mockService: MockModeService;
 
   beforeEach(async () => {
+    mockService = new MockModeService();
+
     await TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [NavbarComponent],
+      providers: [{provide: ModeService, useValue: mockService}]
     })
     .compileComponents();
 
@@ -20,4 +34,41 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call toggleMode and handleMode method of ModeService', () => {
+    const handleModeSpy = jest.spyOn(mockService, 'handleMode');
+    component.toggleMode();
+    expect(handleModeSpy).toHaveBeenCalled();
+  });
+
+  describe('ngOnChanges', () => {
+    it('should call updateIcon method', () => {
+      const updateIconSpy = jest.spyOn(component, 'updateIcon');
+      component.selectedTopic = 'HTML';
+      component.ngOnChanges();
+      expect(updateIconSpy).toHaveBeenCalled();
+    });
+
+    it('should update topicIcon correctly based on selectedTopic', () => {
+      const topics = {
+        HTML: 'html',
+        CSS: 'css',
+        JavaScript: 'javascript',
+        Accessibility: 'accessibility',
+      };
+
+      for (const [topic, icon] of Object.entries(topics)) {
+        component.selectedTopic = topic;
+        component.ngOnChanges();
+        expect(component.topicIcon).toBe(icon);
+      }
+    });
+
+    it('should set topicIcon to empty string for unknown topics', () => {
+      component.selectedTopic = 'Unknown';
+      component.ngOnChanges();
+      expect(component.topicIcon).toBe('');
+    });
+  });
+  
 });
