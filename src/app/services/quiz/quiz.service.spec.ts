aa0c0a524f1f3f2158aab75a2e65af4fb265f783
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-07-22 00:04:59 +0200

    added test for quizservice component

--- src/app/services/quiz/quiz.service.spec.ts
+++ src/app/services/quiz/quiz.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuizService } from './quiz.service';
+import { QUIZZES } from '../../db-data';
 
 describe('QuizService', () => {
   let service: QuizService;
@@ -12,5 +13,20 @@ describe('QuizService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return questions for a given topic', () => {
+    const questions = service.getQuestions('JavaScript');
+    expect(questions).toEqual(QUIZZES[0].questions);
+  });
+
+  it('should return an empty array for a non-existing topic', () => {
+    const questions = service.getQuestions('NonExistentTopic');
+    expect(questions.length).toBe(0);
+  });
+
+  it('should be case insensitive when matching topics', () => {
+    const questions = service.getQuestions('javascript');
+    expect(questions).toEqual(QUIZZES[0].questions);
   });
 });
